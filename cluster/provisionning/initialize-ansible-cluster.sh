5e519ef13bb0c45d397e49a52a09152922f95e9d
dykoffi <dykoffi2000@gmail.com>
2023-02-09 23:20:03 +0000

    Update initialize ansible script following oci-env exigences

--- cluster/provisionning/initialize-ansible-cluster.sh
+++ cluster/provisionning/initialize-ansible-cluster.sh
@@ -1,26 +1,23 @@
 # PUBLIC IP
-master_ansible_public_ip="35.238.67.220"
-server_1_public_ip="35.225.145.114"
-server_2_public_ip="34.71.123.252"
-server_3_public_ip="34.173.168.89"
-server_4_public_ip="35.232.251.129"
-server_5_public_ip="35.192.211.36"
+control_plane_1_public_ip="35.238.67.220"
+data_plane_1_public_ip="34.71.123.252"
+data_plane_2_public_ip="35.225.145.114"
+data_plane_3_public_ip="35.192.211.36"
+master_ansible_public_ip="34.173.168.89"
 
 ssh-keygen -f "~/.ssh/known_hosts" -R $master_ansible_public_ip &
-ssh-keygen -f "~/.ssh/known_hosts" -R $server_1_public_ip &
-ssh-keygen -f "~/.ssh/known_hosts" -R $server_2_public_ip &
-ssh-keygen -f "~/.ssh/known_hosts" -R $server_3_public_ip &
-ssh-keygen -f "~/.ssh/known_hosts" -R $server_4_public_ip &
-ssh-keygen -f "~/.ssh/known_hosts" -R $server_5_public_ip &
+ssh-keygen -f "~/.ssh/known_hosts" -R $control_plane_1_public_ip &
+ssh-keygen -f "~/.ssh/known_hosts" -R $data_plane_1_public_ip &
+ssh-keygen -f "~/.ssh/known_hosts" -R $data_plane_2_public_ip &
+ssh-keygen -f "~/.ssh/known_hosts" -R $data_plane_3_public_ip &
 
 wait
 
 ssh-keyscan -H $master_ansible_public_ip >> ~/.ssh/known_hosts &
-ssh-keyscan -H $server_1_public_ip >> ~/.ssh/known_hosts &
-ssh-keyscan -H $server_2_public_ip >> ~/.ssh/known_hosts &
-ssh-keyscan -H $server_3_public_ip >> ~/.ssh/known_hosts &
-ssh-keyscan -H $server_4_public_ip >> ~/.ssh/known_hosts &
-ssh-keyscan -H $server_5_public_ip >> ~/.ssh/known_hosts &
+ssh-keyscan -H $control_plane_1_public_ip >> ~/.ssh/known_hosts &
+ssh-keyscan -H $data_plane_1_public_ip >> ~/.ssh/known_hosts &
+ssh-keyscan -H $data_plane_2_public_ip >> ~/.ssh/known_hosts &
+ssh-keyscan -H $data_plane_3_public_ip >> ~/.ssh/known_hosts &
 
 wait
 
@@ -44,21 +41,19 @@ echo
 read ENTRER
 
 # Ajouter la clé publique du master ansible aux clés autorisées des nodes ansible
-ssh k8s@$server_1_public_ip "echo $master_ansible_pub_key > ~/.ssh/authorized_keys" &
-ssh k8s@$server_2_public_ip "echo $master_ansible_pub_key > ~/.ssh/authorized_keys" &
-ssh k8s@$server_3_public_ip "echo $master_ansible_pub_key > ~/.ssh/authorized_keys" &
-ssh k8s@$server_4_public_ip "echo $master_ansible_pub_key > ~/.ssh/authorized_keys" &
-ssh k8s@$server_5_public_ip "echo $master_ansible_pub_key > ~/.ssh/authorized_keys" &
+ssh k8s@$control_plane_1_public_ip "echo $master_ansible_pub_key > ~/.ssh/authorized_keys" &
+ssh k8s@$data_plane_1_public_ip "echo $master_ansible_pub_key > ~/.ssh/authorized_keys" &
+ssh k8s@$data_plane_2_public_ip "echo $master_ansible_pub_key > ~/.ssh/authorized_keys" &
+ssh k8s@$data_plane_3_public_ip "echo $master_ansible_pub_key > ~/.ssh/authorized_keys" &
 
 wait
 
 # Ajouter les adresses ip des nodes ansibles aux hotes connues du master pour faciliter la connexion sans interruption
 ssh k8s@$master_ansible_public_ip "
-ssh-keyscan -H server-1 > ~/.ssh/known_hosts && \
-ssh-keyscan -H server-2 >> ~/.ssh/known_hosts && \
-ssh-keyscan -H server-3 >> ~/.ssh/known_hosts && \
-ssh-keyscan -H server-4 >> ~/.ssh/known_hosts && \
-ssh-keyscan -H server-5 >> ~/.ssh/known_hosts
+ssh-keyscan -H control-plane-1 > ~/.ssh/known_hosts && \
+ssh-keyscan -H data-plane-1 >> ~/.ssh/known_hosts && \
+ssh-keyscan -H data-plane-2 >> ~/.ssh/known_hosts && \
+ssh-keyscan -H data-plane-3 >> ~/.ssh/known_hosts
 "
 
 # Ajouter l'dresse de github.com pour faciliter la connexion sans interruption
@@ -67,6 +62,7 @@ ssh k8s@$master_ansible_public_ip "ssh-keyscan -H github.com >> ~/.ssh/known_hos
 # # Cloner le depot rke2
 ssh k8s@$master_ansible_public_ip "
 git clone git@github.com:data354/ds54.git && \
+cd ds54 && git checkout oci-env && cd - && \
 cp -rd ds54/cluster/rke2 rke2 && \
 rm -rdf ds54
 "
