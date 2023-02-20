2fe3bee55d7ade7edf5462003145a52815abc176
dykoffi <dykoffi2000@gmail.com>
2023-02-20 11:05:36 +0000

    Del airflow appp

--- cluster/provisionning/initialize-ansible-cluster.sh
+++ cluster/provisionning/initialize-ansible-cluster.sh
@@ -1,9 +1,9 @@
 # PUBLIC IP
-control_plane_1_public_ip="34.133.54.202"
-data_plane_1_public_ip="34.69.103.222"
-data_plane_2_public_ip="34.27.200.3"
-data_plane_3_public_ip="35.226.214.183"
-master_ansible_public_ip="34.134.27.71"
+control_plane_1_public_ip="34.71.233.130"
+data_plane_1_public_ip="34.27.149.152"
+data_plane_2_public_ip="104.197.62.158"
+data_plane_3_public_ip="35.192.41.41"
+master_ansible_public_ip="34.29.76.254"
 
 ssh-keygen -f "~/.ssh/known_hosts" -R $master_ansible_public_ip &
 ssh-keygen -f "~/.ssh/known_hosts" -R $control_plane_1_public_ip &
@@ -22,14 +22,14 @@ ssh-keyscan -H $data_plane_3_public_ip >> ~/.ssh/known_hosts &
 wait
 
 # Generer la paire de clé ssh du master ansible
-ssh k8s@$master_ansible_public_ip "echo -e 'y\n' | ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''"
+ssh data354@$master_ansible_public_ip "echo -e 'y\n' | ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''"
 
-# ssh k8s@$server_public_ip "echo -e 'y\n' | ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''"
-# ssh k8s@$server_1_public_ip "echo -e 'y\n' | ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''"
-# ssh k8s@$server_2_public_ip "echo -e 'y\n' | ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''"
+# ssh data354@$server_public_ip "echo -e 'y\n' | ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''"
+# ssh data354@$server_1_public_ip "echo -e 'y\n' | ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''"
+# ssh data354@$server_2_public_ip "echo -e 'y\n' | ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''"
 
 # Recuperer la clé publique du master ansible
-master_ansible_pub_key=$(ssh k8s@$master_ansible_public_ip "cat ~/.ssh/id_rsa.pub")
+master_ansible_pub_key=$(ssh data354@$master_ansible_public_ip "cat ~/.ssh/id_rsa.pub")
 
 echo
 echo "Veuillez ajouter la clé du master ansible à votre compte github."
@@ -41,15 +41,15 @@ echo
 read ENTRER
 
 # Ajouter la clé publique du master ansible aux clés autorisées des nodes ansible
-ssh k8s@$control_plane_1_public_ip "echo $master_ansible_pub_key > ~/.ssh/authorized_keys" &
-ssh k8s@$data_plane_1_public_ip "echo $master_ansible_pub_key > ~/.ssh/authorized_keys" &
-ssh k8s@$data_plane_2_public_ip "echo $master_ansible_pub_key > ~/.ssh/authorized_keys" &
-ssh k8s@$data_plane_3_public_ip "echo $master_ansible_pub_key > ~/.ssh/authorized_keys" &
+ssh data354@$control_plane_1_public_ip "echo $master_ansible_pub_key > ~/.ssh/authorized_keys" &
+ssh data354@$data_plane_1_public_ip "echo $master_ansible_pub_key > ~/.ssh/authorized_keys" &
+ssh data354@$data_plane_2_public_ip "echo $master_ansible_pub_key > ~/.ssh/authorized_keys" &
+ssh data354@$data_plane_3_public_ip "echo $master_ansible_pub_key > ~/.ssh/authorized_keys" &
 
 wait
 
 # Ajouter les adresses ip des nodes ansibles aux hotes connues du master pour faciliter la connexion sans interruption
-ssh k8s@$master_ansible_public_ip "
+ssh data354@$master_ansible_public_ip "
 ssh-keyscan -H control-plane-1 > ~/.ssh/known_hosts && \
 ssh-keyscan -H data-plane-1 >> ~/.ssh/known_hosts && \
 ssh-keyscan -H data-plane-2 >> ~/.ssh/known_hosts && \
@@ -57,10 +57,10 @@ ssh-keyscan -H data-plane-3 >> ~/.ssh/known_hosts
 "
 
 # Ajouter l'dresse de github.com pour faciliter la connexion sans interruption
-ssh k8s@$master_ansible_public_ip "ssh-keyscan -H github.com >> ~/.ssh/known_hosts"
+ssh data354@$master_ansible_public_ip "ssh-keyscan -H github.com >> ~/.ssh/known_hosts"
 
 # # Cloner le depot rke2
-ssh k8s@$master_ansible_public_ip "
+ssh data354@$master_ansible_public_ip "
 git clone git@github.com:data354/ds54.git && \
 cd ds54 && git checkout oci-env && cd - && \
 cp -rd ds54/cluster/rke2 rke2 && \
